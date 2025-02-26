93ebde27889acb46c78dd29b252017ab12c9fc25
Abhishek Patidar <abhipatidar253@gmail.com>
2025-02-26 15:27:44 +0530

    Port No change

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,13 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
 app.use('/api/tasks', taskRoutes);
 app.use('/api/categories', categoryRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
